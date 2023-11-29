e115983e36d0ee63e145fb3241a4a571512fd994
SANDEEP KUMAR SAROJ <42493758+sandeep7722@users.noreply.github.com>
2023-11-29 17:24:53 +0000

    covered comparision

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score="33";
 
-console.log(typeof score);//string
-//or - both same
-console.log(typeof(score));//string
+// console.log(typeof score);//string
+// //or - both same
+// console.log(typeof(score));//string
 
 
 let valueInNumber=Number(score);
-console.log(valueInNumber);//33
-console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//33
+// console.log(typeof valueInNumber);//number
 
 //"33" => 33
 //"33abc" => NaN
@@ -16,7 +16,7 @@ console.log(typeof valueInNumber);//number
 let isLoggedIn=0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);//false
+// console.log(booleanIsLoggedIn);//false
 
 // 1 => true
 //0 => false
@@ -25,11 +25,28 @@ console.log(booleanIsLoggedIn);//false
 
 let someNumber= 45
 let stringNumber=String(someNumber);
-console.log(stringNumber);//45
-console.log(typeof stringNumber);// string
+// console.log(stringNumber);//45
+// console.log(typeof stringNumber);// string
 
 let posValue=1
 
 let negValue=-posValue
-console.log(negValue)
+// console.log(negValue)
+
+// console.log(2+3)
+// console.log(2*3)
+
+let str1="hello"
+let str2="sandeep"
+
+let str3=str1+str2
+
+// console.log(str3)//hellosandeep
+
+// console.log("1"+2)//12
+// console.log(1+"2")//12
+
+// console.log("1"+2+2)//122
+
+// console.log(1+2+"2")//32
 
