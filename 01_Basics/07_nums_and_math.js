e35208a88df6e27c3f6ad56ef56db95767dc05f6
SANDEEP KUMAR SAROJ <42493758+sandeep7722@users.noreply.github.com>
2023-11-30 10:11:48 +0000

    covered numbers and math concepts

---
+++ 01_Basics/07_nums_and_math.js
@@ -0,0 +1,43 @@
+const score=400
+// console.log(score);//400
+
+const balance=new Number(100);
+// console.log(balance);//[Number:100]
+
+// console.log(balance.toString());//100
+
+// console.log(balance.toString().length);//3
+// console.log(balance.toFixed(2));//100.00
+
+const otherNumber=123.8959
+
+// console.log(otherNumber.toPrecision(3));//124
+// console.log(otherNumber.toPrecision(4));//123.9
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));//10,00,000
+
+//------------------Math----------------
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.2));//5
+// console.log(Math.floor(4.9));//4
+// console.log(Math.min(4,1,2,5));
+// console.log(Math.max(4,5,2,7,3,9));
+
+
+// console.log(Math.random())//value bw 0 and 1
+
+// console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
