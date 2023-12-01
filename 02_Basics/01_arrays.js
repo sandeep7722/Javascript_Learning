423be9b237b3a39dd820497ad2af714cb6ca6549
SANDEEP KUMAR SAROJ <42493758+sandeep7722@users.noreply.github.com>
2023-12-01 04:52:35 +0000

    covered array topics

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -24,6 +24,22 @@ myArr.shift();//delete 1 element from starting
 // console.log(myArr.indexOf(3));//2
 const newArr=myArr.join();
 
-console.log(myArr);
-console.log(newArr);
-console.log(typeof newArr);//string
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);//string
+
+//slice, splice
+
+console.log("A", myArr);//A [ 1, 2, 3, 4, 5, true, 'sandeep' ]
+
+const myn1=myArr.slice(1,3)//print ele from index 1 to index 2
+
+console.log(myn1);//[2,3]
+
+
+console.log("B",myArr);//B [ 1, 2, 3, 4, 5, true, 'sandeep' ]
+
+const myn2=myArr.splice(1,3);//print and remove ele from index 1 to index 3 from myArr
+console.log(myArr);//[ 1, 5, true, 'sandeep' ]
+console.log(myn2);//[ 2, 3, 4 ]
+
